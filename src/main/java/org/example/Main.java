6d47c185487f887d10c0ac0bfdba94d7f3d93888
mariatsarouche <it2021100@hua.gr>
2024-11-10 21:04:33 +0200

    added in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Γράψε το όνομα σου.");
+        System.out.println("Γράψε το όνομα σου (in english please).");
 
         Scanner in = new Scanner(System.in);
         String name = in.nextLine();
