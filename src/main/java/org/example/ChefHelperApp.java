0d1f28189ac3811d1a7d1313cf53361375f89308
mariatsarouche <it2021100@hua.gr>
2024-11-19 22:20:45 +0200

    added a class

--- src/main/java/org/example/ChefHelperApp.java
+++ src/main/java/org/example/ChefHelperApp.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class ChefHelperApp {
     public static void main(String[] args) {
 
-        String fileName = "ChefHelper/src/main/resources/" + args[0];
+        String fileName = "ChefHelper/src/main/resources/";
 
         // Ελέγχουμε αν δόθηκε αρχείο ως όρισμα
         if (args.length != 1) {
