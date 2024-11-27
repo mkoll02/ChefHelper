5aced7386bf163e9a314bed7142eac59a87bb643
iwannaradi <ioannaradi2003@gmail.com>
2024-11-27 18:09:28 +0200

    Fixed Time and Utensils

--- src/main/java/org/example/Time.java
+++ src/main/java/org/example/Time.java
@@ -6,23 +6,23 @@ public class Time extends Display {
     @Override
     public void display(String recipe) {
         // Εντοπισμός δεικτών του συμβόλου '~'
-        List<Integer> indexes = indexes("~", recipe);
-
-        // Εξαγωγή χρόνων
-        List<String> times = isolateString(recipe, indexes, "{", "}");
-
-        int totalTime = 0; // Συνολικός χρόνος
-
-
-        for (String time : times) {
-            System.out.println("- " + time.trim());
-
-            // Υπολογισμός χρόνου: αφαιρούμε τις μονάδες (π.χ., %minutes) και προσθέτουμε τον αριθμό
-            totalTime += extractTimeInMinutes(time.trim());
-        }
+//        List<Integer> indexes = indexes("~", recipe);
+//
+//        // Εξαγωγή χρόνων
+//        List<String> times = isolateString(recipe, indexes, "{", "}");
+//
+//        int totalTime = 0; // Συνολικός χρόνος
+//
+//
+//        for (String time : times) {
+//            System.out.println("- " + time.trim());
+//
+//            // Υπολογισμός χρόνου: αφαιρούμε τις μονάδες (π.χ., %minutes) και προσθέτουμε τον αριθμό
+//            totalTime += extractTimeInMinutes(time.trim());
+//        }
 
         // Εμφάνιση του συνολικού χρόνου
-        System.out.println("Συνολική ώρα: " + totalTime + " λεπτά.");
+        //System.out.println("Συνολική ώρα: " + totalTime + " λεπτά.");
     }
 
     // Εξάγει τον χρόνο σε λεπτά από το κείμενο (π.χ., "15%minutes").
--- src/main/java/org/example/Utensils.java
+++ src/main/java/org/example/Utensils.java
@@ -5,15 +5,15 @@ import java.util.List;
 public class Utensils extends Display {
     @Override
     public void display(String recipe) {
-        // Εντοπισμός δεικτών του συμβόλου '#'
-        List<Integer> indexes = indexes("#", recipe);
-
-        // Εξαγωγή εργαλείων
-        List<String> utensils = isolateString(recipe, indexes, " ", ",");
-
-        System.out.println("Σκεύη:");
-        for (String utensil : utensils) {
-            System.out.println("- " + utensil.trim());
-        }
-    }
+//        // Εντοπισμός δεικτών του συμβόλου '#'
+//        List<Integer> indexes = indexes("#", recipe);
+//
+//        // Εξαγωγή εργαλείων
+//        List<String> utensils = isolateString(recipe, indexes, " ", ",");
+//
+//        System.out.println("Σκεύη:");
+//        for (String utensil : utensils) {
+//            System.out.println("- " + utensil.trim());
+//        }
+  }
 }
