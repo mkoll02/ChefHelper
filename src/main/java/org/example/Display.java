b0f32e998d2ab5e9d8f7fbbbe995c65fe4ffce1a
iwannaradi <ioannaradi2003@gmail.com>
2024-11-27 17:25:43 +0200

    Time and Utensils

--- src/main/java/org/example/Display.java
+++ src/main/java/org/example/Display.java
@@ -2,14 +2,18 @@ package org.example;
 
 public abstract class Display extends RecipeProcessor {
     //override this in inheritors to print there
-    public void display(String recipe) {}
+    public void display(String recipe) {
+    }
 
-   public static Display[] printer() {
-       Display[] display = new Display[4];
-       display[0] = new Ingredients();
-       display[1] = new Utensils();
-       display[2] = new Time();
-       display[3] = new Steps();
-       return display;
-   }
+    public static Display[] printer() {
+        Display[] display = new Display[4];
+        display[0] = new Ingredients();
+        display[1] = new Utensils();
+        display[2] = new Time();
+        display[3] = new Steps();
+        return display;
+    }
+
+
 }
+
