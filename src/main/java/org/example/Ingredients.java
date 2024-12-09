354c52550ca25a55b855a1306465e56bce3520e6
mariatsarouche <it2021100@hua.gr>
2024-12-09 22:36:10 +0200

    completed string proccesing in Ingredients

--- src/main/java/org/example/Ingredients.java
+++ src/main/java/org/example/Ingredients.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ingredients extends Display {
-    List<String> ingredients = new ArrayList<>();
+    List<String> ingredients = new ArrayList<>(); //final list that's printed
     int numberOfPeople;
 
     //lists to process
-    List<String> initial, name, measurement;
-    List<Float> quantity;
-    List<Integer> occurrences;
+    List<String> initial = new ArrayList<>();
+    List<String> name = new ArrayList<>();
+    List<String> measurement = new ArrayList<>();
+    List<Double> quantity = new ArrayList<>();;
+    List<Integer> occurrences = new ArrayList<>();;
 
     @Override
     public void display(String recipe) {
@@ -22,27 +24,52 @@ public class Ingredients extends Display {
         this.numberOfPeople = numberOfPeople;
     }
 
-    public void setIngredients(List<String> ingredients) {//name+quantity+measurement
-        this.ingredients = ingredients;
+    public void setIngredients() {//name+quantity+measurement
+        String a;
+        for(int i=0; i<initial.size(); i++) {
+            a = String.format("%,.2f %s %s", quantity.get(i), measurement.get(i), name.get(i));
+            ingredients.add(a);
+        }
     }
 
     public void printIngredients(String recipe) {
-        for(String ingredient : prepareInitial(recipe)){
+        toPrint(recipe);
+        for(String ingredient : ingredients){
             System.out.println(ingredient);
         }
     }
 
+    public void toPrint(String recipe) {
+        prepareIngredients(recipe);
+        setIngredients();
+//        checkAndSumDuplicates();
+//        conversions();
+//        multiplyForPeople();
+    }
+
     public List<String> prepareInitial(String recipe) { //initial string
         occurrences = indexes("@", recipe);
+        if(occurrences.isEmpty()) throw new IllegalArgumentException("Δεν υπάρχουν υλικά σε αυτή τη συνταγή.");
         return isolateString(recipe, occurrences, "#", "~");
     }
 
-    public void prepareIngredients(String recipe) {//combine arrays into one
+    public void prepareIngredients(String recipe) {
         initial = prepareInitial(recipe);
-//        for(int i=0; i<initial.size(); i++) {
-//
-//        }
+        String i;
+        for (String s : initial) {
+            name.add(extractName(s));
+            i = insideBrackets(s);
+            quantity.add(extractNumberOf(i));
+            measurement.add(extractMeasurement(i));
+        }
     }
+
+    public void checkAndSumDuplicates() {}
+
+    public void conversions() {}
+
+    public void multiplyForPeople() {}
+
 
     public String extractName(String str) {//string before {
         if(str.isEmpty()) return "";
@@ -50,17 +77,20 @@ public class Ingredients extends Display {
     }
 
     public String insideBrackets(String str) {
-        if(str.equals("{}")) return "";
-        return stringIfExists(str, 1, str.indexOf("}"));
+        int start = str.indexOf("{");
+        int end = str.indexOf("}");
+        if(str.equals("{}") || start == -1 || end == -1) return "";
+        return stringIfExists(str, start+1, end);
     }
 
-    public double extractNumberOf(String insideBrackets) { //the number before %
-        if(insideBrackets.isEmpty()) return 1;
+    public Double extractNumberOf(String insideBrackets) { //the number before %
+        if(insideBrackets.isEmpty()) return 1.0;
         String s = stringIfExists(insideBrackets, 0, insideBrackets.indexOf("%"));
         return Double.parseDouble(s);
     }
 
     public String extractMeasurement(String insideBrackets) {//from measurement to end
+        if(!insideBrackets.contains("%")) return "";
         return insideBrackets.substring(insideBrackets.indexOf("%")+1);
     }
 }
